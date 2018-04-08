448b5e84f81e00294bf3d79b8b13060069e99803
tommyEtka <thurma13@gmail.com>
2018-04-08 17:03:09 -0400

    updated methods

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,49 +1,40 @@
 
 
 /**
-
  * IMPORTANT: Make sure you are using the correct package name.
-
+ * <p>
  * This example uses the package name:
-
+ * <p>
  * package com.example.android.justjava
-
+ * <p>
  * If you get an error when copying this code into Android studio, update it to match teh package name found
-
+ * <p>
  * in the project's AndroidManifest.xml file.
-
  **/
-
-
 
 
 package com.example.android.justjava;
 
 
+import android.os.Bundle;
 
+import android.support.v7.app.AppCompatActivity;
 
+import android.view.View;
 
+import android.widget.TextView;
 
-         import android.os.Bundle;
-
-         import android.support.v7.app.AppCompatActivity;
-
-         import android.view.View;
-
-         import android.widget.TextView;
-
-         import java.text.NumberFormat;
+import java.text.NumberFormat;
 
 
 /**
-
  * This app displays an order form to order coffee.
-
  */
 
 public class MainActivity extends AppCompatActivity {
 
-    int quantity=0;
+    int quantity = 0;
+    //int price = 0;
 
     @Override
 
@@ -55,62 +46,70 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Calculates the price of the order
+     *
+     * @return total price
+     */
+
+    public int calculatePrice() {
+        return quantity * 5;
+    }
 
 
     /**
+     * Shows a summary of the order
+     */
 
+    private String createOrderSummary(int price) {
+        String name = "Tometka Thurman";
+        String priceMessage = "Name: " + name + " \n"
+                + "Quantity: " + quantity + "\n"
+                + "Total: $" + calculatePrice();
+        priceMessage += " \nThank you!";
+        return priceMessage;
+    }
+
+
+    /**
      * This method is called when the order button is clicked.
-
      */
 
     public void submitOrder(View view) {
-
-
-        //displayPrice(quantity*5);
-        int price = quantity*5;
-        String priceMessage="Total: $" + price;
-        priceMessage = priceMessage + " \nThank you!";
-        displayMessage(priceMessage);
+        int price = calculatePrice();
+        displayMessage(createOrderSummary(price));
 
     }
 
 
-
     /**
-
      * This method is called when the increment button is clicked.
-
      */
 
     public void increment(View view) {
 
-        quantity=quantity+1;
-        display(quantity);
+        quantity = quantity + 1;
+        displayQuantity(quantity);
 
     }
 
     /**
-
      * This method is called when the decrement button is clicked.
-
      */
 
     public void decrement(View view) {
 
-        quantity=quantity-1;
-        display(quantity);
+        quantity = quantity - 1;
+        displayQuantity(quantity);
 
     }
 
 
-
     /**
-
      * This method displays the given quantity value on the screen.
-
      */
 
-    private void display(int number) {
+    private void displayQuantity(int number) {
 
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
 
@@ -119,9 +118,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-
      * This method displays the given price on the screen.
-
      */
 
     private void displayPrice(int number) {
@@ -134,9 +131,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-
      * This method displays the given text on the screen.
-
      */
 
     private void displayMessage(String message) {
@@ -146,5 +141,6 @@ public class MainActivity extends AppCompatActivity {
         priceTextView.setText(message);
 
     }
+
 
 }
