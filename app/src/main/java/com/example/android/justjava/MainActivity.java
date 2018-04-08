eec9147a8b76edd7014d045cfb73d0f6736f8b3e
tommyEtka <thurma13@gmail.com>
2018-04-08 18:58:09 -0400

    add more functionality

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -17,14 +17,9 @@ package com.example.android.justjava;
 
 
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
-
 import android.view.View;
-
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 
 /**
@@ -117,17 +112,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-
-    private void displayPrice(int number) {
-
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-
-    }
 
 
     /**
@@ -136,9 +120,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayMessage(String message) {
 
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
 
-        priceTextView.setText(message);
+        orderSummaryTextView.setText(message);
 
     }
 
